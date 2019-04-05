444123317315374db22293b57eb88da15e5d805c
Mike Duncan <mduncan2600@gmail.com>
2019-04-05 14:31:54 -0400

    Updates

--- BarstoolWarrierCucumber/src/test/java/cucumber/duncan/LoginPageTests.java
+++ BarstoolWarrierCucumber/src/test/java/cucumber/duncan/LoginPageTests.java
@@ -1,5 +1,79 @@
 package cucumber.duncan;
 
-public class LoginPageTests {
+import cucumber.api.java8.En;
+
+public class LoginPageTests implements En {
+
+	public void Java8VersionSteps() {
+		Given("we are at the login page", () -> {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new cucumber.api.PendingException();
+		});
+
+		When("we login with username mduncan and password Test{int}", (Integer int1) -> {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new cucumber.api.PendingException();
+		});
+
+		Then("we are relocated to Page One", () -> {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new cucumber.api.PendingException();
+		});
+
+		When("we login with username sharford and password Test{int}", (Integer int1) -> {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new cucumber.api.PendingException();
+		});
+
+		When("we login with username tkubin and password Test{int}", (Integer int1) -> {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new cucumber.api.PendingException();
+		});
+
+		When("we login with username eangone and password Test{int}", (Integer int1) -> {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new cucumber.api.PendingException();
+		});
+
+		When("we login with username mduncan and password None{int}", (Integer int1) -> {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new cucumber.api.PendingException();
+		});
+
+		Then("a validation message for an invalid username and password combination is displayed", () -> {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new cucumber.api.PendingException();
+		});
+
+		When("we login with username abcxyz and password Test{int}", (Integer int1) -> {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new cucumber.api.PendingException();
+		});
+
+		When("we login with an empty username field", () -> {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new cucumber.api.PendingException();
+		});
+
+		Then("a validation message is displayed that indicates username is a required field", () -> {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new cucumber.api.PendingException();
+		});
+
+		When("we login with an empty password field", () -> {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new cucumber.api.PendingException();
+		});
+
+		Then("a validation message is displayed that indicates password is a required field", () -> {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new cucumber.api.PendingException();
+		});
+
+		When("we login with an empty username field and an empty password field", () -> {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new cucumber.api.PendingException();
+		});
+	}
 
 }
